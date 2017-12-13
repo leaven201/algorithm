4fd62e19f4402db85caf69befed7911800cf17e2
Lin <505280467@qq.com>
2017-12-13 15:24:41 +0800

    Revert "完成数据结构-2.3.1数组-面试题4：二维数组中的查找"
    This reverts commit 982db485f29b718a42706aa0b8edf61a6642060f.

--- src/niuke/Array.java
+++ src/niuke/Array.java
@@ -16,33 +16,13 @@ public class Array {
  * 在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
  * 请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
  * 
- * 思路：找最右上角的元素，如果该元素大于target则删除该列(column--)，再找一下个右上角 
- *                 如果该元素小于target则删除改行(row++)，再找下一个右上角
- *                 如果该元素等于target则结束return：true 如果矩阵空了则return：false
+ * 思路：找最右上角的元素，如果该元素大于target则删除该列，再找一下个右上角 如果该元素小于target则删除改行，再找下一个右上角
+ * 如果该元素等于target则结束return：true 如果矩阵空了则return：false
  */
 
 class Solution {
 	public boolean Find(int target, int[][] array) {
-		int row=0;
-		int column=array[0].length-1;
-		return Find(target, array, row, column);		
-	}
-	public boolean Find(int target,int[][] array,int row,int column) {
-		if(row>array.length-1||column<0)
-			return false;
-		if(array[row][column]==target){
-			return true;
-		}
-		else if(array[row][column]>target) {
-			column--;
-			return Find(target, array, row, column);
-		}
-		else {
-			row++;
-			return Find(target, array, row, column);
-		}
+
+		
 	}
 }
-
-
-
